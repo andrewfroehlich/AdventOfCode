6e42aa5c52df21d360a0783b40e9a741b858fa9b
Andrew F <andrew.r.froehlich@gmail.com>
2020-12-10 18:51:25 +0000

    Add Problem 10 Java

---
+++ Java/Problem10.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+import java.time.*;
+
+class Problem10 {
+    public static void main(String args[]){
+        File file = new File("/home/ec2-user/environment/AOC/Resources/problem10.txt");
+        
+        try{
+            ArrayList<Integer> vals = buildList(file);
+            Instant i2 = Instant.now();
+            System.out.println("Part 1: "+part1(vals));
+            Instant i3 = Instant.now();
+            System.out.println("Part 2: "+part2(vals));
+            Instant i4 = Instant.now();
+            System.out.println(String.format("Part 1: %s, Part 2: %s", 
+                Duration.between(i2, i3).toMillis(),
+                Duration.between(i3, i4).toMillis()));
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public static ArrayList<Integer> buildList(File file) throws Exception{
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        ArrayList<Integer> vals = new ArrayList<Integer>();
+        String current;
+        while ((current = (br.readLine())) != null) {
+            vals.add(Integer.parseInt(current));
+        }
+        return vals;
+    }
+    
+    public static int part1(ArrayList<Integer> vals) throws Exception{
+        Collections.sort(vals);
+        int previousValue = 0;
+        int oneJumps = 0;
+        //the last value is 3 more than max, so add one to threeJumps (start at 1)
+        int threeJumps = 1;
+        for(int i : vals){
+            if(i - 3 == previousValue){
+                threeJumps++;
+            } else if(i - 1 == previousValue){
+                oneJumps++;
+            }
+            previousValue = i;
+        }
+        
+        return oneJumps * threeJumps;
+    }
+    
+    public static long part2(ArrayList<Integer> vals) throws Exception{
+        //add 0 as the first item in the list
+        vals.add(0, 0);
+        
+        HashMap<Integer,Long> indexToPathCount = new HashMap<Integer,Long>();
+        // Max value is valid
+        indexToPathCount.put(vals.size() - 1, new Long(1));
+        for(int i = vals.size() - 2; i >= 0; i--){
+            long pathsFromCurrentIndex = 0;
+            
+            //check the next three indices to add paths
+            int currentVal = vals.get(i);
+            for(int j = i+1; j <= i+3; j++){
+                if(j < vals.size() && currentVal+3 >= vals.get(j)){
+                    pathsFromCurrentIndex += indexToPathCount.get(j);
+                }
+            }
+            indexToPathCount.put(i, pathsFromCurrentIndex);
+        }
+        
+        return indexToPathCount.get(0);
+    }
+}
