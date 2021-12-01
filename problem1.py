357d3e042485f4772233843cb2b43f8d320ad0b8
andrewfroehlich <58447130+andrewfroehlich@users.noreply.github.com>
2021-12-01 09:08:02 -0500

    Adding input file, and cleaning up code file

--- problem1.py
+++ problem1.py
@@ -1,44 +1,40 @@
 
 def part1():
-    f = open("C:\\Users\\fandrew\\Desktop\\AOC2021\\input1.txt")
+    f = open("input1.txt")
+    currInt = answer = 0
+    prev = 100000
     current = f.readline().strip()
-    currentInt = 0
-    last = 100000
-    answer = 0
-    while(current is not None and current != ''):
-        currentInt = int(current)
-        if currentInt > last:
-            answer = answer+1
-        last = currentInt
-        current = f.readline().strip()
+    while(current != ''):
+        currInt = int(current)
+        if currInt > prev:
+            answer += 1
+        prev = currInt
+        current = f.readline()
     return str(answer)
 
 print("1: "+part1())
 
 def part2():
-    f = open("C:\\Users\\fandrew\\Desktop\\AOC2021\\input1.txt")
-    current = f.readline().strip()
-    currentInt = 0
-    lastWindow = 100000
-    currentWindow = 0
-    answer = 0
+    f = open("input1.txt")
+    currInt = currWindow = answer = 0
+    prevWindow = 100000
     
-    windows = [int(current)]
-    current = f.readline().strip()
-    while(current is not None and current != ''):
-        currentInt = int(current)
+    windows = [int(f.readline())]
+    current = f.readline()
+    while(current != ''):
+        currInt = int(current)
         if len(windows) > 2:
-            currentWindow = windows.pop(0)
-            if currentWindow > lastWindow:
-                answer = answer+1
-            lastWindow = currentWindow
+            currWindow = windows.pop(0)
+            if currWindow > prevWindow:
+                answer += 1
+            prevWindow = currWindow
         for i in range(len(windows)):
-            windows[i] = windows[i] + currentInt
-        windows.append(currentInt)
-        current = f.readline().strip()
+            windows[i] = windows[i] + currInt
+        windows.append(currInt)
+        current = f.readline()
     # Need one more run as the last loop "completes" a windows
-    if windows.pop(0) > lastWindow:
-        answer = answer+1
+    if windows.pop(0) > prevWindow:
+        answer += 1
     return str(answer)
 
 print("2: "+part2())
