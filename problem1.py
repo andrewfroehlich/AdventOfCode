10ae1d0d5e5dfc0239b91349783e3d0c1a49bf5d
Andrew Froehlich <58447130+andrewfroehlich@users.noreply.github.com>
2021-12-02 10:29:00 -0500

    Clean up file iteration

--- problem1.py
+++ problem1.py
@@ -1,34 +1,27 @@
 def part1():
     f = open("input1.txt")
-    currInt = answer = 0
+    answer = 0
     prev = 100000
-    current = f.readline().strip()
-    while(current != ''):
-        currInt = int(current)
-        if currInt > prev:
+    for line in f:
+        if int(line) > prev:
             answer += 1
-        prev = currInt
-        current = f.readline()
+        prev = int(line)
     return answer
 
 def part2():
     f = open("input1.txt")
-    currInt = currWindow = answer = 0
+    currWindow = answer = 0
     prevWindow = 100000
-    
-    windows = [int(f.readline())]
-    current = f.readline()
-    while(current != ''):
-        currInt = int(current)
+    windows = []
+    for line in f:
         if len(windows) > 2:
             currWindow = windows.pop(0)
             if currWindow > prevWindow:
                 answer += 1
             prevWindow = currWindow
         for i in range(len(windows)):
-            windows[i] += currInt
-        windows.append(currInt)
-        current = f.readline()
+            windows[i] += int(line)
+        windows.append(int(line))
     # Need one more run as the last loop "completes" a windows
     if windows.pop(0) > prevWindow:
         answer += 1
