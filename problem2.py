6f29e80c92bc31e9148f285797f685bca2092153
Andrew Froehlich <58447130+andrewfroehlich@users.noreply.github.com>
2021-12-02 10:21:26 -0500

    Clean up file iterator

--- problem2.py
+++ problem2.py
@@ -1,28 +1,24 @@
 def part1():
     f = open("input2.txt")
     depth = horiz = 0
-    current = f.readline()
-    while(current != ''):
-        command = current.split(' ')
+    for line in f:
+        command = line.split(' ')
         if command[0] == "forward":
             horiz += int(command[1])
         else:
             depth += (1 if command[0] == "down" else -1) * int(command[1])
-        current = f.readline()
     return horiz*depth
 
 def part2():
     f = open("input2.txt")
     depth = horiz = aim = 0
-    current = f.readline()
-    while(current != ''):
-        command = current.split(' ')
+    for line in f:
+        command = line.split(' ')
         if command[0] == "forward":
             horiz += int(command[1])
             depth += aim * int(command[1])
         else:
             aim += (1 if command[0] == "down" else -1) * int(command[1])
-        current = f.readline()
     return horiz*depth
 
 print("Part 1:",part1())
