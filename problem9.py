1864855e4ef6d0145d0286b02bbb278e0b2ee34a
Andrew Froehlich <58447130+andrewfroehlich@users.noreply.github.com>
2021-12-09 13:50:38 -0500

    Spacing and line breaks

--- problem9.py
+++ problem9.py
@@ -5,10 +5,10 @@ def part1():
     for i in range(len(lines)):
         for j in range(len(lines[i])):
             value = int(lines[i][j])
-            if (i==0 or int(lines[i-1][j])>value) and \
-              (i==len(lines)-1 or int(lines[i+1][j])>value) and \
-              (j==0 or int(lines[i][j-1])>value) and \
-              (j==len(lines[i])-1 or int(lines[i][j+1])>value):
+            if (i == 0 or int(lines[i-1][j]) > value) and \
+              (i == len(lines)-1 or int(lines[i+1][j]) > value) and \
+              (j == 0 or int(lines[i][j-1]) > value) and \
+              (j == len(lines[i])-1 or int(lines[i][j+1]) > value):
                 answer += 1 + value
     return answer
 
@@ -18,7 +18,8 @@ def part2():
     seen = set()
     
     def area(x,y):
-        if x<0 or x>=len(lines) or y<0 or y>=len(lines[x]) or (x,y) in seen or lines[x][y] == "9":
+        if x<0 or x>=len(lines) or y<0 or y>=len(lines[x]) or \
+          ((x,y) in seen) or lines[x][y] == "9":
             return 0
         seen.add((x,y))
         return 1 + area(x+1,y) + area(x-1,y) + area(x,y-1) + area(x,y+1)
