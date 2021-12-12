5bce6809ec60b2516b3717dbbfecfcc9e6ffe340
Andrew Froehlich <58447130+andrewfroehlich@users.noreply.github.com>
2021-12-12 12:40:55 -0500

    Update lowercase check for clarity

--- problem12.py
+++ problem12.py
@@ -4,14 +4,14 @@ f = open("input12.txt")
 edges = defaultdict(list)
 for line in f:
     points = line.strip().split('-')
-    if points[0] != "end" and points[1] != "start":
+    if points[0] != "end" and points[1] != "start": #nothing toward start or from end
         edges[points[0]].append(points[1])
-    if points[1] != "end" and points[0] != "start":
+    if points[1] != "end" and points[0] != "start": #nothing toward start or from end
         edges[points[1]].append(points[0])
 
 def traverse(current, visited, revisited):
-    if not current[0].isupper():
-        visited.add(current) #only say visited if small cave
+    if current.islower():
+        visited.add(current) #only 'visit' if on a small cave
     if current == "end":
         global paths
         paths += 1
