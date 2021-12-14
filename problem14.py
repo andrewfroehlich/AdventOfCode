7b14cbd4f02721ef364f76dd826ae8cb83e54025
Andrew Froehlich <58447130+andrewfroehlich@users.noreply.github.com>
2021-12-14 12:58:52 -0500

    Better method using pairs, wrong file last commit

--- problem14.py
+++ problem14.py
@@ -1,121 +1,43 @@
-import sys
+from collections import defaultdict
 
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-class linkedList:
-    def __init__(self):
-        self.head=None
+def run(steps):
+    t,pairs = open("input14.txt").read().split("\n\n")
+    polymers = dict((i for i in l.split(" -> ")) for l in pairs.strip().split('\n'))
+    paircount = defaultdict(int)
 
-t,pairs = open("input14.txt").read().split("\n\n")
-polymers = dict((i for i in l.split(" -> ")) for l in pairs.strip().split('\n'))
-template = linkedList()
-current = None
-for c in t:
-    if current is None:
-        current = Node(c)
-        template.head = current
-    else:
-        t = Node(c)
-        current.next = t
-        current = t
+    #convert input to pairs 
+    for index in range(len(t)-1):
+        paircount[t[index]+t[index+1]] += 1
 
-#build set of all possible chars
-allchars = set()
-for key in polymers:
-    allchars.add(key[0])
-    allchars.add(key[1])
-    allchars.add(polymers[key])
-print("Total keys",len(allchars))
-
-#build 20-step cache of counts, e.g. for CV in 20 steps, you result in XXX C, YYY V, etc.
-cache20 = dict()
-for key in polymers:
-    templist = linkedList()
-    templist.head = Node(key[0])
-    templist.head.next = Node(key[1])
+    #run steps
     step = 0
-    while step < 20:
-        current = templist.head
-        nextNode = current.next
-        while nextNode is not None:
-            insert = Node(polymers[current.data+nextNode.data])
-            current.next = insert
-            insert.next = nextNode
-            current = nextNode
-            nextNode = current.next
+    while step < steps:
+        newpaircount = defaultdict(int)
+        for key in paircount:
+            if paircount[key] > 0:
+                newpaircount[key[0]+polymers[key]] += paircount[key]
+                newpaircount[polymers[key]+key[1]] += paircount[key]
+        paircount = newpaircount
         step += 1
-    
-    counts = dict()
-    current = templist.head
-    while current is not None:
-        if current.data in counts:
-            counts[current.data] += 1
-        else:
-            counts[current.data] = 1
-        current = current.next
-    cache20[key] = counts
-    print(key," cache count added")
 
-# Run 20 steps of the original input
-step = 0
-while step < 20:
-    current = template.head
-    nextNode = current.next
-    while nextNode is not None:
-        insert = Node(polymers[current.data+nextNode.data])
-        current.next = insert
-        insert.next = nextNode
-        current = nextNode
-        nextNode = current.next
-    step += 1
-    print("Step",step,"complete")
+    #enumerate all possible chars
+    allchars = set()
+    for key in polymers:
+        allchars.add(key[0])
+        allchars.add(key[1])
+        allchars.add(polymers[key])
 
-# For each 2-char sequence in the resulting 20 step string, add the counts of running an additional 20 steps
-countsSum = dict()
-for c in allchars:
-    countsSum[c] = 0
-current = template.head
-nextNode = current.next
-while nextNode is not None:
-    key = current.data+nextNode.data
-    current = nextNode
-    nextNode = current.next
+    #find min/max of all chars
+    maxCount = minCount = -1
     for c in allchars:
-        countsSum[c] = countsSum[c] + cache20[key][c] - (1 if key[1] == c and nextNode is not None else 0) #remove double counting
-print("Counts Sum Complete")
-
-# Print all counts and the Part 2 answer
-maxCount = minCount = -1
-for c in allchars:
-    print(c,countsSum[c])
-    maxCount = countsSum[c] if countsSum[c] > maxCount else maxCount
-    minCount = countsSum[c] if (countsSum[c] < minCount or minCount == -1) else minCount
-print("Part 2:",maxCount-minCount)
-
-
-#V 439051102821
-#P 977393099208
-#N 3067784388562
-#H 3800433128803
-#S 619697444903
-#C 2804420769138
-#K 1634200947813
-#B 1382279882429
-#O 3898229386832
-#F 2267250700179
-#Part 2: 3459178284011
-
-#right:
-F 2267248527101
-K 1634199382184
-C 2804418100771
-O 3898225660655
-B 1382278561174
-N 3067781482737
-P 977392169054
-H 3800429511266
-V 439050679634
-S 619696853169
-Part 2: 3459174981021
+        count = 0
+        for key in paircount:
+            if c == key[0]:
+                count += paircount[key]
+        count += 1 if t[-1] == c else 0
+        maxCount = count if count > maxCount else maxCount
+        minCount = count if (count < minCount or minCount == -1) else minCount
+    return maxCount-minCount
+    
+print("Part 1:",run(10))
+print("Part 2:",run(40))
