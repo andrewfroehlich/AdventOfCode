6e070313507a3b37c5384166656f079a1b58cd71
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2022-01-11 10:59:09 -0500

    Redid problem 8 in Python, went from 145 lines of Java to 52 of Python

---
+++ 2020/Python/problem8.py
@@ -0,0 +1,52 @@
+lines = open("../Resources/problem8.txt").readlines()
+
+def runInstruction(index,acc,swapIndex):
+    instruction = lines[index].strip().split()
+    value = int(instruction[1][1:]) * (1 if instruction[1][0:1]=="+" else -1)
+    if instruction[0] == "jmp":
+        if swapIndex == index:
+            index += 1
+        else:
+            index += value
+    else:
+        if swapIndex == index:
+            index += value
+        else:
+            index += 1
+            if instruction[0] == "acc":
+                acc += value
+    return index,acc
+
+def part1():
+    acc = index = 0
+    visited = set()
+    while True:
+        if index in visited:
+            return acc
+        visited.add(index)
+        index,acc = runInstruction(index,acc,-1)
+
+def part2():
+    #find all indices of the loop
+    loopIndices = set()
+    acc = index = 0
+    visited = set()
+    while index not in loopIndices:
+        if index in visited:
+            loopIndices.add(index)
+        visited.add(index)
+        index,acc = runInstruction(index,acc,-1)
+    #iterate over the loop indices swapping nop/jmp until hitting the end
+    for swapIndex in loopIndices:
+        if lines[swapIndex].strip().split()[0] == "acc":
+            continue
+        acc = index = 0
+        visited = set()
+        while index not in visited:
+            visited.add(index)
+            index,acc = runInstruction(index,acc,swapIndex)
+            if index == len(lines):
+                return acc
+
+print("Part 1:",part1())
+print("Part 2:",part2())
