0c926438a524abec0d44516ace3560d89a78d4d2
Andrew Froehlich <58447130+andrewfroehlich@users.noreply.github.com>
2022-04-07 07:28:22 -0400

    Clean up 1

--- 2019/problem1.py
+++ 2019/problem1.py
@@ -1,13 +1,9 @@
-mass = 0
-for line in open("Resources/input1.txt"):
-    mass += int(line)//3 - 2
-print("Part 1:",mass)
+processed_lines = list((int(d)//3 - 2) for d in open("Resources/input1.txt"))
+print("Part 1:",sum(processed_lines))
 
 mass = 0
-for line in open("Resources/input1.txt"):
-    my_mass = int(line)//3 - 2
+for my_mass in processed_lines:
     while my_mass > 0:
         mass += my_mass
         my_mass = my_mass//3 - 2
 print("Part 2:",mass)
-    
