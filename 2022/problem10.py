79bf125f855dd29e92abbc6e83354bec32166744
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2022-12-10 13:22:36 -0500

    2022 P10 - slight refactor

--- 2022/problem10.py
+++ 2022/problem10.py
@@ -1,21 +1,21 @@
-addx = extra_cycles = part1 = 0
+addx = part1 = 0
+extra_cycle = False
 cycle = X = 1
 part2 = [["." for _ in range(40)] for _ in range(6)]
 
 for line in open("input10.txt"):
     if line.strip() != "noop":
         addx = int(line.strip().split()[1])
-        extra_cycles = 1
-    for i in range(extra_cycles+1):
-        if (cycle-20) % 40 == 0 and cycle < 221:
+        extra_cycle = True
+    for _ in range(2 if extra_cycle else 1):
+        if (cycle-20) % 40 == 0:
             part1 += cycle*X
         if X-1 <= ((cycle-1) % 40) <= X+1:
             part2[(cycle-1)//40][(cycle-1)%40] = "#"
         cycle += 1
-    extra_cycles = 0
-    if addx != 0:
-        X += addx
-        addx = 0
+    X += addx
+    addx = 0
+    extra_cycle = False
 
 print("Part 1:",part1,"\nPart 2:")
 for i in range(len(part2)):
