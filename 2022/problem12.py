9d7013f0478bac37b44429c231b4f96c8b9dc125
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2022-12-12 17:30:51 -0500

    minor cleanup

--- 2022/problem12.py
+++ 2022/problem12.py
@@ -13,22 +13,18 @@ for i in range(len(lines)):
     for j in range(len(lines[i])):
         if lines[i][j] == "E":
             end = (i,j)
-            break
-    if end != (-1,-1):
-        break
 
-directions = [(-1,0),(0,1),(0,-1),(1,0)]
-bfs = deque( [(end[0], end[1], 0)] )
+bfs = deque( [(end[0], end[1], 0)] ) # i, j, steps
 visited = set( [(end[0], end[1])] )
 part2 = -1
-while len(bfs) > 0:
+while bfs:
     i, j, steps = bfs.popleft()
     if part2 == -1 and (lines[i][j] == 'a' or lines[i][j] == 'S'):
         part2 = steps
     if lines[i][j] == 'S':
         print("Part 1:",steps)
         break
-    for x,y in directions:
+    for x,y in [(-1,0),(0,1),(0,-1),(1,0)]:
         if 0 <= i+x < len(lines) and 0 <= j+y < len(lines[0]) and (i+x,j+y) not in visited and elevation(lines[i+x][j+y])+1 >= elevation(lines[i][j]):
             visited.add( (i+x, j+y) )
             bfs.append( (i+x, j+y, steps+1) )
