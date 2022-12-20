24ac834f57d68479b2554ff9103a9cdf4e5c2285
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2022-12-20 12:03:47 -0500

    slight cleanup

--- 2022/problem20.py
+++ 2022/problem20.py
@@ -1,25 +1,24 @@
 def solve(dec_key=1,loops=1):
-    i = 0
+    identifier = 0
     nums,order = [],[]
     zero_obj = None
     for line in open("input20.txt").read().splitlines():
-        ob = (int(line)*dec_key,i)
+        ob = (int(line)*dec_key, identifier)
         if ob[0] == 0:
             zero_obj = ob
         nums.append(ob)
         order.append(ob)
-        i += 1
+        identifier += 1
     
     for loop in range(loops):
         for i in range(len(order)):
             num_index = nums.index(order[i])
             num = nums.pop(num_index)
-            new_index = (num_index + num[0] + len(nums)) % len(nums)
-            nums.insert(new_index, num)
+            nums.insert(((num_index + num[0] + len(nums)) % len(nums)), num)
 
     ans = 0
     zero_index = nums.index(zero_obj)
-    for j in range(1000,3001,1000):
+    for j in (1000,2000,3000):
         ans += nums[((zero_index + j)%len(nums))][0]
     return ans
 
