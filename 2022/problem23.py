c5101d6c094e84bdfd790dda6dc473c2df3471c9
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2022-12-23 09:57:22 -0500

    minor cleanup

--- 2022/problem23.py
+++ 2022/problem23.py
@@ -6,33 +6,24 @@ def find_halt():
         for i in range(len(lines[j])):
             if lines[j][i] == "#":
                 elves.add( (i,j) )
-    
+    # d is proposals in-order, where the first coord of the tuple is the move-to position, but all 3 are checked
     d = [((0,-1),(-1,-1),(1,-1)), ((0,1),(-1,1),(1,1)), ((-1,0),(-1,-1),(-1,1)), ((1,0),(1,-1),(1,1))]
     adj = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
     r = 0
     while True:
-        prop = dict() #key is proposal, value is previous position
+        prop = dict() #key is proposal, value is previous position; includes elves that don't move
         collisions = set() #saves collisions in case there are more than 2 colliding on a spot
         
         for x,y in elves:
             # check if there is any adjacent elf
-            adj_elf = False
-            for i,j in adj:
-                if (x+i,y+j) in elves:
-                    adj_elf = True
-                    break
-            if not adj_elf:
+            if sum( ((x+i,y+j) in elves) for i,j in adj ) == 0:
                 prop[ (x,y) ] = (x,y)
             else:
                 #propose movement
                 proposal_added = False
                 for p in range(len(d)):
                     check = d[(r+p)%len(d)]
-                    check_fail = False
-                    for i,j in check:
-                        if (x+i,y+j) in elves:
-                            check_fail = True
-                    if not check_fail:
+                    if sum( ((x+i,y+j) in elves) for i,j in check ) == 0:
                         proposal_added = True
                         proposal = x+check[0][0],y+check[0][1]
                         if proposal not in prop and proposal not in collisions:
@@ -53,7 +44,7 @@ def find_halt():
             #find min/max coordinates for smallest rectangle
             min_x,max_x = min(elves,key=lambda item:item[0])[0], max(elves,key=lambda item:item[0])[0]
             min_y,max_y = min(elves,key=lambda item:item[1])[1], max(elves,key=lambda item:item[1])[1]
-            part1 = ((max_x-min_x+1)*(max_y-min_y+1))-len(elves)
+            part1 = ((max_x-min_x+1)*(max_y-min_y+1)) - len(elves)
         
         if sum(key != value for key,value in prop.items()) == 0:
             return part1, r+1 #stopped moving
