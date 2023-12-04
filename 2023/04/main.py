51bc9d3a7bc49d2b31b74bd57978fa070fa462b6
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2023-12-04 13:53:26 +0000

    4: more minor code cleanup

--- 2023/04/main.py
+++ 2023/04/main.py
@@ -5,21 +5,19 @@ def solve():
     sum1 = sum2 = 0
     bonus_buffer = deque()
     for line in open("input.txt"):
-        nums = line.strip().split(": ")[1].split(" | ")
+        nums = line.split(": ")[1].split(" | ")
         winners = set([int(d) for d in re.findall("\\d+", nums[0])])
         found = sum([1 for n in re.findall("\\d+", nums[1]) if int(n) in winners])
         
         # Part 1
-        sum1 += 2 ** (found-1) if found > 0 else 0
+        sum1 += 2 ** (found - 1) if found > 0 else 0
         
         # Part 2 
-        multiple = 1
-        if bonus_buffer:
-            multiple += bonus_buffer.popleft()
+        multiple = 1 + (bonus_buffer.popleft() if bonus_buffer else 0)
         sum2 += multiple
         for i in range(found):
             if len(bonus_buffer) > i:
-                bonus_buffer[i] = bonus_buffer[i] + multiple
+                bonus_buffer[i] += multiple
             else:
                 bonus_buffer.append(multiple)
     return sum1,sum2
