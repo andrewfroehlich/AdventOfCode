808dc120c8c98faeb9e006ff6d21338badd52d11
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2023-12-05 20:28:36 +0000

    5: minor code cleanup

--- 2023/05/main.py
+++ 2023/05/main.py
@@ -1,9 +1,12 @@
-def part1():
+def parseFile():
     with open('input.txt', 'r') as f:
         sections = f.read().split("\n\n")
-    values = [int(s) for s in sections[0].split(": ")[-1].split()]
+    seeds = [int(s) for s in sections[0].split(": ")[-1].split()]
     sections.pop(0)
+    return seeds,sections
 
+def part1():
+    values,sections = parseFile()
     for section in sections:
         lines = section.splitlines()
         ranges = [tuple(int(d) for d in l.split()) for l in lines[1:]]
@@ -22,10 +25,7 @@ def part1():
     return min(values)
 
 def part2():
-    with open('input.txt', 'r') as f:
-        sections = f.read().split("\n\n")
-    seed_list = [int(s) for s in sections[0].split(": ")[-1].split()]
-    sections.pop(0)
+    seed_list,sections = parseFile()
     seeds = [(seed_list[i],seed_list[i+1]) for i in range(0,len(seed_list),2)]
     
     mappings = []
@@ -33,26 +33,24 @@ def part2():
         lines = section.splitlines()
         ranges = [tuple(int(d) for d in l.split()) for l in lines[1:]]
         mappings.append(ranges)
-
-    found = -1
+    
     location = 0 #starting at location 0, brute force until we find a valid seed
-    while found < 0:
+    while True:
         current = location
         for map_i in range(len(mappings)-1, -1, -1):
-            found_next = -1
+            found = -1
             for dest_start,src_start,length in mappings[map_i]:
                 if dest_start <= current < dest_start + length:
-                    found_next = src_start + (current - dest_start)
+                    found = src_start + (current - dest_start)
                     break #go to next value
-            if found_next == -1:
-                found_next = current #if not found in the mapping, keep the same value
-            current = found_next
+            if found == -1:
+                found = current #if not found in the mapping, keep the same value
+            current = found
         
         for start,rnge in seeds:
             if start <= current < start+rnge:
                 return location
         location += 1
-    return -1
 
 if __name__ == '__main__':
     print("Part 1:",part1())
