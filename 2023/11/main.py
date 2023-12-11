353e46f512adf5b2a32239bedf55ab9f1b978f42
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2023-12-11 13:03:44 +0000

    11: code cleanup

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -1,18 +1,13 @@
-def solve(part):
+def solve(expand_factor):
     with open("input.txt", "r") as f:
         lines = f.read().splitlines()
-    exp_factor = 1 if part == 1 else (1000000-1)
-    exp_cols = [c for c in range(len(lines[0])) if len(set([val[c] for val in lines])) == 1]
+    empty_cols = [c for c in range(len(lines[0])) if len(set([val[c] for val in lines])) == 1]
+    empty_rows = [r for r in range(len(lines)) if len(set(lines[r])) == 1]
     points = []
-    row = 0
-    for r in lines:
-        found = False
-        for c in range(len(r)):
-            if r[c] =="#":
-                points.append( (row,c + sum(exp_factor for e in exp_cols if e<c)) )
-                found = True
-        row = row+1 if found else row+1+exp_factor
-    
+    for r in range(len(lines)):
+        for c in range(len(lines[r])):
+            if lines[r][c] == "#":
+                points.append( (r + sum(expand_factor for e in empty_rows if e<r),c + sum(expand_factor for e in empty_cols if e<c)) )
     sum1 = 0
     for i in range(len(points)-1):
         for j in range(i+1,len(points)):
@@ -22,4 +17,4 @@ def solve(part):
 
 if __name__ == "__main__":
     print("Part 1:",solve(1))
-    print("Part 2:",solve(2))
+    print("Part 2:",solve(1000000-1))
