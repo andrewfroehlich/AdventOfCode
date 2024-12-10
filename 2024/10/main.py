4aaee0a6638347ed778759b288963414f0d9e43c
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2024-12-10 09:11:12 -0500

    10: minor updates, including removing unneeded visited set

--- 2024/10/main.py
+++ 2024/10/main.py
@@ -2,24 +2,22 @@ from collections import deque
 
 def dfs(grid, i, j):
     visited,nines,part2 = set(),set(),0
-    dfs = deque()
-    dfs.append( (i,j) )
+    dfs = deque([(i,j)])
     while dfs:
         x,y = dfs.popleft()
-        visited.add( (x,y) )
         current = int(grid[x][y])
         if current == 9:
             part2 += 1
             nines.add( (x,y) )
             continue
         for i,j in [(-1,0),(1,0),(0,-1),(0,1)]:
-            if 0<=(x+i)<len(grid) and 0<=(y+j)<len(grid[x+i]) and (x+i,y+j) not in visited and int(grid[x+i][y+j]) == current+1:
+            if 0<=(x+i)<len(grid) and 0<=(y+j)<len(grid[x+i]) and int(grid[x+i][y+j]) == current+1:
                 dfs.append( (x+i,y+j) )
     return len(nines),part2 # part1 val, part2 val
 
 def run():
-    lines = open("input.txt").read().splitlines()
     part1,part2 = 0,0
+    lines = open("input.txt").read().splitlines()
     for x, line in enumerate(lines):
         for y, c in enumerate(line):
             if c == "0":
