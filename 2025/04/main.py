0d6374f5cb8cb2de5d2e4c64edf33ff2027954b8
Andrew Froehlich <andrew.r.froehlich@gmail.com>
2025-12-09 11:12:31 -0500

    4: some cleanup from changing approach, and cleaning some loops

--- 2025/04/main.py
+++ 2025/04/main.py
@@ -1,12 +1,9 @@
-def search(grid,max_x,max_y,max_adjacent,part2=False):
+def search(grid,max_adjacent,part2=False):
     adjacency = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
     found,found_coords = 0,set()
     while True:
         for x,y in grid:
-            adjacent_count = 0
-            for dx,dy in adjacency:
-                if (x+dx,y+dy) in grid:
-                    adjacent_count += 1
+            adjacent_count = sum(1 for dx,dy in adjacency if (x+dx,y+dy) in grid)
             if adjacent_count < max_adjacent:
                 found_coords.add((x,y))
         if not part2:
@@ -18,12 +15,10 @@ def search(grid,max_x,max_y,max_adjacent,part2=False):
         found_coords = set()
 
 if __name__ == "__main__":
-    grid,max_x,max_y = set(),-1,-1
+    grid = set()
     for y,line in enumerate(open("input.txt")):
-        max_y = y
         for x,char in enumerate(line.strip()):
-            max_x = max(max_x,x)
             if char == "@":
                 grid.add((x,y))
-    print("Part 1:",search(grid,max_x,max_y,4))
-    print("Part 2:",search(grid,max_x,max_y,4,True))
+    print("Part 1:",search(grid.copy(),4))
+    print("Part 2:",search(grid.copy(),4,True))
